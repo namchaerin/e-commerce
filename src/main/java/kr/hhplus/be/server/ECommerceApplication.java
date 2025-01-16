e8b9dda6be974ab4dabb60a4521f7ccfb9e54e5f
namchaerin <cr434972@gmail.com>
2025-01-17 05:20:20 +0900

    feat: service name 변경

--- src/main/java/kr/hhplus/be/server/ECommerceApplication.java
+++ src/main/java/kr/hhplus/be/server/ECommerceApplication.java
@@ -2,14 +2,16 @@ package kr.hhplus.be.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@ServletComponentScan
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = {"kr.hhplus.be.server.domain", "kr.hhplus.be.server.infrastructure"})
-public class ServerApplication {
+public class ECommerceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
+		SpringApplication.run(ECommerceApplication.class, args);
 	}
 
 }
--- src/test/java/kr/hhplus/be/server/ECommerceApplicationTests.java
+++ src/test/java/kr/hhplus/be/server/ECommerceApplicationTests.java
@@ -2,10 +2,9 @@ package kr.hhplus.be.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 
 @SpringBootTest
-class ServerApplicationTests {
+class ECommerceApplicationTests {
 
 	@Test
 	void contextLoads() {
