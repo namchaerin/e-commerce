e3557eb0c4fc900f4521ee8e9a4ba8401334defc
namchaerin <cr434972@gmail.com>
2025-02-21 04:34:30 +0900

    feat: Kafka 테스트 설정 추가

--- src/test/java/kr/hhplus/be/server/TestContainersConfiguration.java
+++ src/test/java/kr/hhplus/be/server/TestContainersConfiguration.java
@@ -3,12 +3,17 @@ package kr.hhplus.be.server;
 import jakarta.annotation.PreDestroy;
 import org.springframework.context.annotation.Configuration;
 import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.kafka.ConfluentKafkaContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @Configuration
 public class TestContainersConfiguration {
 
 	public static final MySQLContainer<?> MYSQL_CONTAINER;
+	public static final ConfluentKafkaContainer KAFKA_CONTAINER = new ConfluentKafkaContainer(
+			DockerImageName.parse("confluentinc/cp-kafka:latest")
+					.asCompatibleSubstituteFor("apache/kafka")
+	);
 
 	static {
 		MYSQL_CONTAINER = new MySQLContainer<>(DockerImageName.parse("mysql:8.0"))
@@ -20,12 +25,23 @@ public class TestContainersConfiguration {
 		System.setProperty("spring.datasource.url", MYSQL_CONTAINER.getJdbcUrl() + "?characterEncoding=UTF-8&serverTimezone=UTC");
 		System.setProperty("spring.datasource.username", MYSQL_CONTAINER.getUsername());
 		System.setProperty("spring.datasource.password", MYSQL_CONTAINER.getPassword());
+
+		//kafka
+		KAFKA_CONTAINER
+				.withEnv("KAFKA_LISTENERS", "PLAINTEXT://:9092,BROKER://:9093,CONTROLLER://:9094")
+				.start();
+		KAFKA_CONTAINER.start();
+		System.setProperty("spring.kafka.bootstrap-servers", KAFKA_CONTAINER.getBootstrapServers());
 	}
 
 	@PreDestroy
 	public void preDestroy() {
 		if (MYSQL_CONTAINER.isRunning()) {
 			MYSQL_CONTAINER.stop();
+		}
+
+		if (KAFKA_CONTAINER.isRunning()) {
+			KAFKA_CONTAINER.stop();
 		}
 	}
 
